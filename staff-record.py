d96ccaee29f4652ebb22621260e87db7972a915d
brown-son <brown-son@users.noreply.github.com>
2020-04-02 13:34:42 +0100

    snippets

--- staff-record.py
+++ staff-record.py
@@ -1,4 +1,30 @@
 #%%
+import datetime
+
+class Staff:
+    """ Plant Staff Class """
+    def __init__ (self,fullname,employment_date,unit,id):
+        self.fullname = fullname
+        self.employment_date = employment_date
+        self.unit = unit
+        self.id = id
+
+ekebo_etuk = Staff('Ekebo Brown Etuk','2018,9,1','ICT','IPC\\202\\2018')
+employed = datetime.datetime.strptime(ekebo_etuk.employment_date,'%Y,%m,%d')
+#print(f"{employed.date(): %d-%b-%y}")
+
+with open('Family.txt',newline='') as fam:
+    f = fam.readline()
+    while f:
+        values = f.split(',')
+        for i, n in enumerate(values):
+            if(i==0):
+                print(f'{n:>5}',end='')
+            else:
+                print(f'{n:<18}',end='')
+        print('\n')
+        f = fam.readline()
+
 staff_details = {
     'name':'',
     'id':'IPC/',
@@ -15,8 +41,4 @@ staff_details = {
 ekebo_etuk_details = ['Ekebo Brown Etuk','IPC/2018/223','Grade 08 Level 05','2018,9,1','Engineering','ICT','Isreal Ekanim','None','None',['22 Days Annual, 7 Days Casual & Sick leave',300000,1000000,122000]]
 
 ekebo_etuk = dict.fromkeys(staff_details.keys())
-
-print(ekebo_etuk)
-print('\n')
-
 # %%
