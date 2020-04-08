8fb0740d3a7117ca67380e428a278ca5b29a7908
brown-son <brown-son@users.noreply.github.com>
2020-04-08 13:43:53 +0100

    more snippets

--- import_csv.py
+++ import_csv.py
@@ -7,13 +7,15 @@ name_list = open('name_list.csv', encoding='utf-8', newline='')
 with name_list as nl:
     nl.readline()
     reader = enumerate(csv.reader(nl))
+    print('\n')
     for i, row in reader:
         #print(i, row)
         name = row[0].split(' ')
         first_name = name[0].strip()
         last_name = name[1].strip()
         phone_nos = row[1]
-        print(last_name +', ' + first_name + ' ->> ' + phone_nos)
+        fullname_sur = last_name +', ' + first_name
+        print(f'{fullname_sur:<40}', phone_nos)
 
 #print('Done')
 #print('File is closed? ', nl.closed)
@@ -26,9 +28,9 @@ with open('zbar_acct_stmt.csv', encoding='utf-8', newline='') as zas:
     reader = enumerate(csv.reader(zas))
     zas.readline()
     heading = f"""
-    \n{'S/N':^5}{'TRANS DATE':<20}{'INCOME':>12}{'DESCRIPTION':^17}{'EXPENSES':>12}{'DESCRIPTION':^17}{'BALANCE':>14}"""
+    \n{'S/N':^5}{'TRANS DATE':<20}{'INCOME':>12}{'DESCRIPTION':^20}{'EXPENSES':>12}{'DESCRIPTION':^20}{'BALANCE':>14}"""
     print(heading)
-    print('-'*98)
+    print('-'*110)
     total_income = 0
     total_expense = 0
     for i, row in reader:
@@ -48,11 +50,12 @@ with open('zbar_acct_stmt.csv', encoding='utf-8', newline='') as zas:
         f_expense = '₦'+f'{expense:,.2f}'
         f_bbf = '₦'+f'{bbf:,.2f}'
         trans_date = f'{trans_date:%B %d, %Y}'
-        print(f'{i:>5}', f'{trans_date:<20}', f'{f_income:>10}', f'{inc_desc[0:14]:<15}..', f'{f_expense:>10}', f'{exp_desc[0:14]:<15}..', f'{f_bbf:>12}')
+        print(f'{i+1:>5}', f'{trans_date:<20}', f'{f_income:>10}', f'{inc_desc[0:18]:<20}', f'{f_expense:>10}', f'{exp_desc[0:18]:<20}', f'{f_bbf:>12}')
 
-print('\nTotal Income = ', f'₦{total_income:,.2f}')
-print('Total Expenses = ', f'₦{total_expense:,.2f}')
-print('Balance = ', f'₦{total_income-total_expense:,.2f}')
+print('\n')
+print(f"{'Total Income = ':>20}", f'₦{total_income:,.2f}')
+print(f"{'Total Expenses = ':>20}", f'₦{total_expense:,.2f}')
+print(f"{'Balance = ':>20}", f'₦{total_income-total_expense:,.2f}')
     
 
 
